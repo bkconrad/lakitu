bb1a45999d801d6a1b9a06d4489a99a77fa6ed91
Bryan Conrad <bkconrad@gmail.com>
2016-01-15 12:58:18 -0800

    first instance wrangling

--- lib/lakitu/providers/aws.rb
+++ lib/lakitu/providers/aws.rb
@@ -1,8 +1,28 @@
 require 'provider'
-require 'aws-sdk-core'
+require 'aws-sdk-resources'
 require 'iniparse'
+
 class Lakitu::Provider::Aws < Lakitu::Provider
   def profiles
     IniParse.parse(File.read(File.expand_path('~/.aws/credentials'))).to_hash.keys
+  end
+
+  def instances profile
+    ec2(profile, 'us-east-1').instances.to_a.map { |x| to_hash x }
+  end
+
+  private
+
+  def ec2 profile, region
+    ::Aws::EC2::Resource.new(region: region, profile: profile)
+  end
+
+  def to_hash instance
+    {
+      id: instance.id,
+      name: instance.tags.select do |x| x.key == 'Name' end.first.value,
+      key: instance.key_name,
+      public_ip: instance.public_ip_address
+    }
   end
 end
--- spec/providers/aws_spec.rb
+++ spec/providers/aws_spec.rb
@@ -10,13 +10,52 @@ aws_secret_access_key = SECRETKEY
 region = us-east-1
 EOF
 
+require 'json'
 require 'providers/aws'
 RSpec.describe Lakitu::Provider::Aws do
   subject { Lakitu::Provider::Aws.new }
-  it "gets a list of profiles" do
-    expect(File).to receive(:read).with(File.expand_path('~/.aws/credentials')).and_return AWS_CREDENTIALS_CONTENT
-    expect(subject.profiles).to eq ['default', 'client1']
+  context "with a valid credentials config" do
+    before :each do
+      expect(File).to receive(:read).with(File.expand_path('~/.aws/credentials')).and_return AWS_CREDENTIALS_CONTENT
+    end
+
+    it "gets a list of profiles" do
+      expect(subject.profiles).to eq ['default', 'client1']
+    end
   end
 
-  it "gets a list of instances for each profile"
+  context "with a given list of instances" do
+    before do
+      Aws.config[:ec2] = {
+        stub_responses: {
+          describe_instances: {
+            reservations: [{
+              instances: [
+                { instance_id: 'i-deadbeef', public_ip_address: '1.2.3.4', key_name: 'testkey', tags: [ { key: 'Name', value: 'test' } ] },
+                { instance_id: 'i-beeff00d', public_ip_address: '1.2.3.5', key_name: 'testkey2', tags: [ { key: 'Name', value: 'test2' } ] }
+              ]}
+            ]
+          }
+        }
+      }
+    end
+
+    it "gets a list of instances for a given profile" do
+      expected = [
+        {
+          id: 'i-deadbeef',
+          name: 'test',
+          key: 'testkey',
+          public_ip: '1.2.3.4'
+        },
+        {
+          id: 'i-beeff00d',
+          name: 'test2',
+          key: 'testkey2',
+          public_ip: '1.2.3.5'
+        }
+      ]
+      expect(subject.instances('epoxy').to_json).to eq(expected.to_json)
+    end
+  end
 end
